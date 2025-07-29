b995634e795b93030773d39268630f05932c96b9
Muhammed Emin BALCI <102964743+Mebalci@users.noreply.github.com>
2025-07-30 02:07:56 +0300

    Delete test_urun.py

--- test_urun.py
+++
@@ -1,6 +0,0 @@
-# test_urun.py
-from app.services.urun_tarayici import urunleri_tarama
-
-url = "https://www.trendyol.com/sr?mid=254250&os=1"
-veri = urunleri_tarama(url)
-print(f"{len(veri)} ürün bulundu")
