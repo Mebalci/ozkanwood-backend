050c61afb91ba466c3a26c7e581e1fbe01de70b4
Muhammed Emin BALCI <102964743+Mebalci@users.noreply.github.com>
2025-07-30 17:50:15 +0300

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,11 +1,9 @@
 #!/bin/bash
 set -e
-echo "🔧 Paketler kuruluyor..."
+echo "🚀 Paketler yükleniyor..."
 pip install --upgrade pip
 pip install --use-feature=2020-resolver -r requirements.txt
 
 echo "🎭 Chromium indiriliyor..."
-# PLAYWRIGHT_BROWSERS_PATH zaten env var ile tanımlı
 python -m playwright install chromium
-
 echo "✅ Build tamamlandı."
