c09f117a724bf59b6912522949c6d51ffd51f0d5
Muhammed Emin BALCI <102964743+Mebalci@users.noreply.github.com>
2025-07-30 17:58:48 +0300

    Delete build.sh

--- build.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-set -e
-echo "🚀 Paketler yükleniyor..."
-pip install --upgrade pip
-pip install --use-feature=2020-resolver -r requirements.txt
-
-echo "🎭 Chromium indiriliyor..."
-python -m playwright install chromium
-echo "✅ Build tamamlandı."
